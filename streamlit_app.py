48da6a80a6aadb91de0c86377b95d0e65bf5cc54
jiaweil6 <ljw20050117@gmail.com>
2024-12-17 15:48:09 -0500

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="My App",
+    page_icon="🎉",
+    layout="wide",
+    initial_sidebar_state="collapsed",  # Optional: Collapse the sidebar
+)
+
 st.title("David Audio")
 st.write(
     "Convolution"
 )
+
