dc5c5037cc1e0f92157e7b2a60a9b48f363c6ebe
jiaweil6 <ljw20050117@gmail.com>
2024-12-18 02:53:12 -0500

    sidebar

--- pages/blog2.py
+++ pages/blog2.py
@@ -1,7 +1,9 @@
 import streamlit as st
-
+from sidebar import sidebar
 
 st.title("Blog Post #2: Binaural Beats")
 st.write("""
 Exploring the world of binaural beats and their impact on relaxation and focus.
 """)
+
+sidebar()
--- pages/blog3.py
+++ pages/blog3.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+from sidebar import sidebar
+
 st.title("Blog Post #3: Headphones vs. Earbuds")
 st.write("""
 A comparison between headphones and earbuds to determine which is better for different scenarios.
 """)
+
+sidebar()
+        
