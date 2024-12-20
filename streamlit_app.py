48f6685b788d50582d03cc35246f9c84365d4446
jiaweil6 <ljw20050117@gmail.com>
2024-12-20 17:32:01 -0500

    header

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,28 +9,16 @@ with open( "style.css" ) as css:
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
-      </li>
-    </ul>
-  </div>
+<nav class="navbar fixed-top navbar-expand-lg" style="background-color: #1F2833;">
+    <div style="display: flex; justify-content: center; width: 100%; padding: 1rem;">
+        <h1 style="margin: 0; color: #66FCF1; font-family: 'Courier Prime';">David Audio</h1>
+    </div>
 </nav>
 """, unsafe_allow_html=True)
 
-st.title("David Audio")
+# Add some padding to prevent content from being hidden behind the navbar
+st.markdown('<div style="margin-top: 100px;"></div>', unsafe_allow_html=True)
+
 st.write("Welcome to David Audio, a space where we delve into music, soundscapes, and audio equipment. "
         "Choose a blog post below or learn more about me.")
 
