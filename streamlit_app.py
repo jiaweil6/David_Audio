8270a939fee90d86d6082b3f11ecfa536951d3cb
jiaweil6 <ljw20050117@gmail.com>
2024-12-20 17:05:08 -0500

    font

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from sidebar import sidebar
 
-with open( "style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+def load_css():
+    with open('style.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+# Call this function at the start of your app
+load_css()
 
-st.title("David Audio")
+st.markdown("<h1 style='color: #66FCF1;'>Your Title Here</h1>", unsafe_allow_html=True)
 st.write("Welcome to David Audio, a space where we delve into music, soundscapes, and audio equipment. "
         "Choose a blog post below or learn more about me.")
 
