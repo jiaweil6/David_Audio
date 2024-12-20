af9adac9bd59628d3c508c42f594cb7604b2877c
jiaweil6 <ljw20050117@gmail.com>
2024-12-20 17:18:10 -0500

    header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from sidebar import sidebar
 with open( "style.css" ) as css:
     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
+st.header("David Audio")
+
 
 st.title("David Audio")
 st.write("Welcome to David Audio, a space where we delve into music, soundscapes, and audio equipment. "
