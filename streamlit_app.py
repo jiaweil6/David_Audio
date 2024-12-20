2144fb7a90bb00704d8ca707d94302ac1270c1d9
jiaweil6 <ljw20050117@gmail.com>
2024-12-20 17:26:10 -0500

    header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ with open( "style.css" ) as css:
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
+sidebar()
+
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
   <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
@@ -32,4 +34,4 @@ st.title("David Audio")
 st.write("Welcome to David Audio, a space where we delve into music, soundscapes, and audio equipment. "
         "Choose a blog post below or learn more about me.")
 
-sidebar()
+
