b4075cd15791d3467e3518eb6695dc0cdc7c60fb
jiaweil6 <ljw20050117@gmail.com>
2024-12-21 01:11:45 -0500

    font

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,10 @@ st.markdown('<div style="margin-top: 50px;"></div>', unsafe_allow_html=True)
 
 st.title("Welcome to **DAVID AUDIO** 🎧")
 
+st.markdown("""
+    <div style="margin-top: 20px;"></div>
+""")
+
 st.write("""
 A space where we break down **audio synthesis** and **audio processing** into easy-to-understand demonstrations,  
 showing how theory directly applies to real-world audio applications.  
