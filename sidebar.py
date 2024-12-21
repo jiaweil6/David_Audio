f4b2668ab7353cf99b63ad2adc13e5d1d2a8b236
jiaweil6 <ljw20050117@gmail.com>
2024-12-21 02:28:13 -0500

    fixing

--- sidebar.py
+++ sidebar.py
@@ -4,5 +4,5 @@ def sidebar():
     st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
     st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
     st.sidebar.page_link("pages/blog1.py", label="Convolution", icon="🎧")
-    st.sidebar.page_link("pages/blog2.py", label="Blog 2", icon="🎧")
+    st.sidebar.page_link("pages/blog2.py", label="something", icon="🎧")
     st.sidebar.page_link("pages/blog3.py", label="Blog 3", icon="🎧")
