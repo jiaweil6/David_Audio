f3db44012c4eba824b70aea71bbf495d5211177d
jiaweil6 <ljw20050117@gmail.com>
2024-12-21 04:09:13 -0500

    wow

--- pages/about.py
+++ pages/about.py
@@ -38,7 +38,7 @@ st.markdown("""
             <i class="bi bi-linkedin"></i>
         </button>
     </a>
-    <a href="/static/Resume.pdf" target="_blank" download>
+    <a href="static/Resume.pdf" target="_blank">
         <button>
             <i class="bi bi-file-earmark-person"></i>
         </button>
