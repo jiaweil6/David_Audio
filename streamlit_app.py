91562fccb2b7257dc79da5ff4c81114fb3161d7b
jiaweil6 <ljw20050117@gmail.com>
2024-12-20 22:51:40 -0500

    style

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,18 +12,9 @@ main_body_logo = "images/icon.png"
 st.logo(sidebar_logo, size="large", icon_image=main_body_logo)
 
 # Add some padding to prevent content from being hidden behind the navbar
-st.markdown('<div style="margin-top: 100px;"></div>', unsafe_allow_html=True)
+st.markdown('<div style="margin-top: 50px;"></div>', unsafe_allow_html=True)
 
 st.write("Welcome to David Audio, a space where we delve into music, soundscapes, and audio equipment. "
         "Choose a blog post below or learn more about me.")
-
-# Add footer
-st.markdown("""
-<footer class="footer">
-    <div class="footer-content">
-        <p>Send an email to <a href="mailto:contribute@audiocommunity.com">contribute@audiocommunity.com</a> if you want to contribute to the audio community.</p>
-    </div>
-</footer>
-""", unsafe_allow_html=True)
 
 
