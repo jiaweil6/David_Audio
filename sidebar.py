1c699778d4a4ef91cf9b8c603629e697213fc391
jiaweil6 <ljw20050117@gmail.com>
2024-12-21 02:24:30 -0500

    about section

--- sidebar.py
+++ sidebar.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def sidebar():
-    st.sidebar.page_link("streamlit_app.py", label="Home", icon="🎧")
-    st.sidebar.page_link("pages/blog1.py", label="Blog 1", icon="🎧")
+    st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
+    st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
+    st.sidebar.page_link("pages/blog1.py", label="Convolution", icon="🎧")
     st.sidebar.page_link("pages/blog2.py", label="Blog 2", icon="🎧")
     st.sidebar.page_link("pages/blog3.py", label="Blog 3", icon="🎧")
