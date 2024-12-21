07f108bd157b52c1c19123861400f834d6dabc4c
jiaweil6 <ljw20050117@gmail.com>
2024-12-21 00:49:05 -0500

    introduction

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,20 @@ st.logo(sidebar_logo, size="large", icon_image=main_body_logo)
 # Add some padding to prevent content from being hidden behind the navbar
 st.markdown('<div style="margin-top: 50px;"></div>', unsafe_allow_html=True)
 
-st.write("Welcome to :[blue]DAVID :[blue]AUDIO, a space where we delve into music, soundscapes, and audio equipment. "
-        "Choose a blog post below or learn more about me.")
+st.write("""
+# Welcome to **DAVID AUDIO** 🎧  
+Your destination for exploring the fascinating world of **audio synthesis** and **audio processing** in real-world applications!
+
+While there’s no shortage of online resources about signals and systems, **practical, audio-focused guidance is scarce.**  
+That’s where **theory meets practice**—helping you bridge the gap and bring your audio ideas to life.  
+
+🎯 **What to explore?**  
+- Dive into curated blog posts below  
+- Learn more about me and my journey  
+
+💡 **Want to contribute?**  
+Email me at [jiaweil6@andrew.cmu.edu](mailto:jiaweil6@andrew.cmu.edu) and share your expertise to enrich the audio community!  
+""")
 
 st.latex(r''' 
     \int_{0}^{\infty} f(x) dx
