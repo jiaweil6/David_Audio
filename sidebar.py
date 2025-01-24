ed3d6bc8d28366ea7b75afe2b40c6ea11f72fa55
jiaweil6 <ljw20050117@gmail.com>
2025-01-24 13:51:04 -0500

    fixed sidebar

--- sidebar.py
+++ sidebar.py
@@ -5,4 +5,4 @@ def sidebar():
     st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
     st.sidebar.markdown("---")
     st.sidebar.page_link("pages/blog1.py", label="Convolution", icon="🎤")
-    st.sidebar.page_link("pages/blog2.py", label="Impulse Response", icon="🎧")
+    st.sidebar.page_link("pages/blog2.py", label="Impulse Response", icon="🎸")
