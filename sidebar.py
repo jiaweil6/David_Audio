e8c39278721646e947e0fe9c542e41a7a2b29391
jiaweil6 <ljw20050117@gmail.com>
2025-07-22 01:06:58 -0400

    testing

--- sidebar.py
+++ sidebar.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 def sidebar():
     with st.sidebar:
-        st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
-        st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
-        st.sidebar.markdown("---")
-        st.sidebar.page_link("pages/convolution.py", label="Convolution", icon="🎤")
-        st.sidebar.page_link("pages/phase_vocoder.py", label="Phase Vocoder", icon="📽️")
-        st.sidebar.page_link("pages/impulse_response.py", label="Impulse Response", icon="🎸")
+        st.page_link("streamlit_app.py", label="Home", icon="🏠")
+        st.page_link("pages/about.py", label="About Me", icon="🙋")
+        st.markdown("---")
+        st.page_link("pages/convolution.py", label="Convolution", icon="🎤")
+        st.page_link("pages/phase_vocoder.py", label="Phase Vocoder", icon="📽️")
+        st.page_link("pages/impulse_response.py", label="Impulse Response", icon="🎸")
