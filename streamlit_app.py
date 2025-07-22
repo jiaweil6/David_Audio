f5ed26ad25ba272384e57b30d459eab339cdb56c
jiaweil6 <ljw20050117@gmail.com>
2025-07-22 12:15:42 -0400

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,13 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-sidebar()
+with st.sidebar:
+        st.page_link("streamlit_app.py", label="Home", icon="🏠")
+        st.page_link("pages/about.py", label="About Me", icon="🙋")
+        st.markdown("---")
+        st.page_link("pages/convolution.py", label="Convolution", icon="🎤")
+        st.page_link("pages/phase_vocoder.py", label="Phase Vocoder", icon="📽️")
+        st.page_link("pages/impulse_response.py", label="Impulse Response", icon="🎸")
 
 main_body_logo = "images/icon.png"
 st.logo(main_body_logo)
