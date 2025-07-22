009d1533b53863d6c09f32dffa6950cbc8e2f89f
jiaweil6 <ljw20050117@gmail.com>
2025-07-22 00:47:40 -0400

    sidebar issue fixed

--- sidebar.py
+++ sidebar.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
+
 def sidebar():
-    st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
-    st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
-    st.sidebar.markdown("---")
-    st.sidebar.page_link("pages/convolution.py", label="Convolution", icon="🎤")
-    st.sidebar.page_link("pages/phase_vocoder.py", label="Phase Vocoder", icon="📽️")
-    st.sidebar.page_link("pages/impulse_response.py", label="Impulse Response", icon="🎸")
+    with st.sidebar:
+        st.sidebar.page_link("streamlit_app.py", label="Home", icon="🏠")
+        st.sidebar.page_link("pages/about.py", label="About Me", icon="🙋")
+        st.sidebar.markdown("---")
+        st.sidebar.page_link("pages/convolution.py", label="Convolution", icon="🎤")
+        st.sidebar.page_link("pages/phase_vocoder.py", label="Phase Vocoder", icon="📽️")
+        st.sidebar.page_link("pages/impulse_response.py", label="Impulse Response", icon="🎸")
